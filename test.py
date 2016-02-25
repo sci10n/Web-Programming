479db2fdb296c2b3941cc829c42fb90ee65e5c3e
krewli <krewli@hotmail.com>
2016-02-25 20:24:03 +0100

    Added tests for signup and signin

---
+++ test.py
@@ -0,0 +1,164 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
+
+import database_helper
+
+MALE = 0
+FEMALE = 1
+
+
+class TwiddlerWelcomeView(unittest.TestCase):
+    def setUp(self):
+        database_helper.init_db()
+        self.driver = webdriver.Firefox()
+        self.driver.get("localhost:5000/")
+        self.driver.implicitly_wait(1)
+
+    def tearDown(self):
+        self.driver.close()
+
+    def signup_user(self, firstname, lastname, gender,
+                    city, country, email,
+                    password, repassword):
+        content = self.driver.find_element_by_id("content")
+        signup_form = content.find_element_by_id("signupform")
+
+        firstname_input = signup_form.find_element_by_id("firstname")
+        firstname_input.clear()
+        firstname_input.send_keys(firstname)
+
+        lastname_input = signup_form.find_element_by_id("lastname")
+        lastname_input.clear()
+        lastname_input.send_keys(lastname)
+
+        gender_select = Select(signup_form.find_element_by_id("gender"))
+        gender_select.select_by_index(gender)
+
+        city_input = signup_form.find_element_by_id("city")
+        city_input.clear()
+        city_input.send_keys(city)
+
+        country_input = signup_form.find_element_by_id("country")
+        country_input.clear()
+        country_input.send_keys(country)
+
+        email_input = signup_form.find_element_by_id("email")
+        email_input.clear()
+        email_input.send_keys(email)
+
+        password_input = signup_form.find_element_by_id("password")
+        password_input.clear()
+        password_input.send_keys(password)
+
+        repassword_input = signup_form.find_element_by_id("repassword")
+        repassword_input.clear()
+        repassword_input.send_keys(repassword)
+        repassword_input.submit()
+
+        time.sleep(1)
+
+    def signin_user(self, username, password):
+        driver = self.driver
+
+        content = driver.find_element_by_id("content")
+        signin_form = content.find_element_by_id("signinform")
+
+        username = signin_form.find_element_by_id("username")
+        username.clear()
+        username.send_keys(username)
+
+        password = signin_form.find_element_by_id("password")
+        password.clear()
+        password.send_keys(password)
+        password.submit()
+
+        time.sleep(1)
+
+    def test_signup(self):
+        self.signup_user(firstname="Kalle",
+                         lastname="Anka",
+                         gender=MALE,
+                         city="AnkeBorg",
+                         country="USA",
+                         email="kalle@anka.borg",
+                         password="ankeborg",
+                         repassword="ankeborg")
+
+        email = self.driver.find_element_by_id("signupform").find_element_by_id("email")
+
+        self.assertEqual(email.get_attribute('value').encode('utf-8'), "")
+
+        self.signup_user(firstname="Kalle",
+                         lastname="Anka",
+                         gender=MALE,
+                         city="AnkeBorg",
+                         country="USA",
+                         email="kalle@anka.borg",
+                         password="ankeborg",
+                         repassword="ankeborg")
+
+        self.assertNotEqual(email.get_attribute('value').encode('utf-8'), "")
+
+    def signin_successful(self):
+        success = False
+
+        try:
+            self.driver.find_element_by_id("content").find_element_by_id("signinform")
+        except NoSuchElementException:
+            success = True
+
+        return success
+
+    def test_signin(self):
+        self.signin_user(username="kalle@anka.borg",
+                         password="ankeborg")
+        self.assertFalse(self.signin_successful())
+
+        self.signup_user(firstname="Kalle",
+                         lastname="Anka",
+                         gender=MALE,
+                         city="AnkeBorg",
+                         country="USA",
+                         email="kalle@anka.borg",
+                         password="ankeborg",
+                         repassword="ankeborg")
+
+        self.signin_user(username="kalle@anka.borg",
+                         password="ankeborg")
+        self.assertTrue(self.signin_successful())
+
+    def test_valid_password(self):
+        self.signup_user(firstname="Kalle",
+                         lastname="Anka",
+                         gender=MALE,
+                         city="AnkeBorg",
+                         country="USA",
+                         email="kalle@anka.borg",
+                         password="ankebor",
+                         repassword="ankebor")
+
+        password = self.driver.find_element_by_id("signupform").find_element_by_id("password")
+
+        self.assertNotEqual(password.get_attribute('value').encode('utf-8'), "")
+
+    def test_valid_email(self):
+        self.signup_user(firstname="Kalle",
+                         lastname="Anka",
+                         gender=MALE,
+                         city="AnkeBorg",
+                         country="USA",
+                         email="kalle@anka",
+                         password="ankebor",
+                         repassword="ankebor")
+
+        email = self.driver.find_element_by_id("signupform").find_element_by_id("email")
+
+        self.assertNotEqual(email.get_attribute('value').encode('utf-8'), "")
+
+
+if __name__ == "__main__":
+    unittest.main()
