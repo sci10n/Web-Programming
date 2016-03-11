89ef73b872933aafb5203ea50c19a37190611e2c
krewli <krewli@hotmail.com>
2016-03-11 16:12:42 +0100

    Reverted bug fix

--- server.py
+++ server.py
@@ -60,7 +60,6 @@ def sign_in_token_POST(email, timestamp, client_hash):
                                        "timestamp": int(timestamp)},
                                  hash=client_hash)) or \
             not valid_timestamp(int(timestamp)):
-        database_helper.sign_out(token)
         return ""
 
     if request.environ.get("wsgi.websocket"):
